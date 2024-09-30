635a00a8381dbd4a0e2eea750acd3d5c96e1bf5e
Aayush716 <148058488+Aayush716@users.noreply.github.com>
2024-09-30 12:50:19 +0530

    Create Design a Stack With Increment Operation.cpp

---
+++ OOPS/Design a Stack With Increment Operation.cpp
@@ -0,0 +1,88 @@
+/*
+Design a stack that supports increment operations on its elements.
+
+Implement the CustomStack class:
+
+CustomStack(int maxSize) Initializes the object with maxSize which is the maximum number of elements in the stack.
+void push(int x) Adds x to the top of the stack if the stack has not reached the maxSize.
+int pop() Pops and returns the top of the stack or -1 if the stack is empty.
+void inc(int k, int val) Increments the bottom k elements of the stack by val. If there are less than k elements in the stack, increment all the elements in the stack.
+ 
+
+Example 1:
+
+Input
+["CustomStack","push","push","pop","push","push","push","increment","increment","pop","pop","pop","pop"]
+[[3],[1],[2],[],[2],[3],[4],[5,100],[2,100],[],[],[],[]]
+Output
+[null,null,null,2,null,null,null,null,null,103,202,201,-1]
+Explanation
+CustomStack stk = new CustomStack(3); // Stack is Empty []
+stk.push(1);                          // stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.pop();                            // return 2 --> Return top of the stack 2, stack becomes [1]
+stk.push(2);                          // stack becomes [1, 2]
+stk.push(3);                          // stack becomes [1, 2, 3]
+stk.push(4);                          // stack still [1, 2, 3], Do not add another elements as size is 4
+stk.increment(5, 100);                // stack becomes [101, 102, 103]
+stk.increment(2, 100);                // stack becomes [201, 202, 103]
+stk.pop();                            // return 103 --> Return top of the stack 103, stack becomes [201, 202]
+stk.pop();                            // return 202 --> Return top of the stack 202, stack becomes [201]
+stk.pop();                            // return 201 --> Return top of the stack 201, stack becomes []
+stk.pop();                            // return -1 --> Stack is empty return -1.
+ 
+
+Constraints:
+
+1 <= maxSize, x, k <= 1000
+0 <= val <= 100
+At most 1000 calls will be made to each method of increment, push and pop each separately.
+*/
+
+//Design a stack from vector ,where i kept p as variable that points to top element
+
+class CustomStack {
+public:
+    vector<int> nums;
+    int n;
+    int p;
+    CustomStack(int maxSize) {
+        this->n = maxSize;
+        nums.resize(n);
+        p=-1;//intially stack pointer points to -1
+    }
+    
+    void push(int x) {
+        if(p<n-1){
+            p++;
+            nums[p] = x;
+        }
+    }
+    
+    int pop() {
+        if(p==-1){
+            return -1;
+        }
+        int val = nums[p];
+        p--;
+        return val;
+    }
+    
+    void increment(int k, int val) {
+        int temp_p = p;//temproary pointer to increment k elements in the stack
+        for(int i=0;i<k;i++){
+            if(i<=p){ //increment bottum k values but must be less than where pointer points
+                nums[i] += val;
+            }
+        }
+    }
+};
+
+/**
+ * Your CustomStack object will be instantiated and called as such:
+ * CustomStack* obj = new CustomStack(maxSize);
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * obj->increment(k,val);
+ */
+
